44535f5fabab32055eb8cd664e8aa2e249d5acb4
zxu43 <zxu43@illinois.edu>
2018-04-09 16:41:07 -0500

    1) Finished Down-casting and Up-casting

--- src/Animal.java
+++ src/Animal.java
@@ -7,25 +7,43 @@ public class Animal {
         System.out.println("I am an animal.");
     }
 
+    public static void teach(Animal animal) {
+        if (animal instanceof Dog) {
+            Dog dog = (Dog) animal; //Down-casting
+            dog.callme();
+        }
+        if (animal instanceof Cat) {
+            Cat cat = (Cat) animal; //Down-casting
+            cat.callme();
+        }
+    }
+
     public static class Dog extends Animal {
         public void callme() {
-            System.out.println("In call me of Dog");
+            System.out.println("Woof Woof");
+        }
+    }
+
+    public static class Cat extends Animal {
+        public void callme() {
+            System.out.println("Meow Meow");
         }
     }
 
     public static void main(String[] args) {
         // Up-casting.
-        Dog dog = new Dog();
-        Animal animal = (Animal) dog;
-        animal.callme();
+        Dog dog = new Dog(); //Declare
+        Animal animal1 = (Animal) dog; //Up-casting
+        animal1.callme(); //Still use the instance/object's method
+        animal1.callanimal(); //Can also use the super type's method
         /*
         Up-casting because Animal is the supertype of Dog
         , this casting is called up-casting.
          */
 
-        Animal animal2 = new Dog();
-        animal2.callme();
-        animal2.callanimal();
+        Animal animal2 = new Dog(); //Up-casting
+        animal2.callme(); //Still use the instance/object's method
+        animal2.callanimal(); //Can also use the super type's method
         /*
         Another type of up-casting;
         The up-casting makes sub-type able to use methods written for super types.
@@ -34,7 +52,33 @@ public class Animal {
         reference class's methods.
          */
 
+        // Down-casting.
+        Animal animal3 = new Dog(); //Up-casting
+        Dog dog1 = (Dog) animal3; //Down-casting
+        dog1.callme();
+        /*
+        The compiler sees that animal3 is actually a Dog, so the casting is successful
+         */
 
+        // Wrong down-casting.
+        /*
+        Dog dog2 = new Animal();
+        Animal is not necessarily a dog
+        Will not compile
+         */
+
+
+        Animal animal4 = new Animal();
+        //Dog dog2 = (Dog) animal4;
+        /*
+        Compiled but with ClassCastException
+        The compiler sees that animal4 is not really a Dog but an Animal
+        */
+
+        Animal animalD = new Dog();
+        Animal animalC = new Cat();
+
+        Animal.teach(animalC);
 
 
     }
